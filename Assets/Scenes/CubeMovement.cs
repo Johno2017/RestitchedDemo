e3a6582aba2ff6afed2fd9f47f27b7c45f38e095
John Ong <john.ong135@gmail.com>
2021-04-23 20:10:50 -0700

    Initial commit

---
+++ Assets/Scenes/CubeMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMovement : MonoBehaviour
+{
+    public float _movementSpeedScalar = 10;
+    public float _movementSpeed = 10.0f;
+    public Rigidbody _rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void AdjustSpeed(float newSpeed)
+    {
+        _movementSpeed = newSpeed * _movementSpeedScalar;
+    }
+
+    void FixedUpdate()
+    {
+        //Movement
+        if (Input.GetKey(KeyCode.W))
+        {
+            _rb.MovePosition(transform.position + (Vector3.up * _movementSpeed * Time.deltaTime));
+        }
+        //Movement
+        if (Input.GetKey(KeyCode.S))
+        {
+            _rb.MovePosition(transform.position + (Vector3.down * _movementSpeed * Time.deltaTime));
+        }
+
+
+    }
+}
